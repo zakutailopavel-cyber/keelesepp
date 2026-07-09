3620564845f1c4f8f4b4d191b3c1d0d71561ba15
zakutailopavel-cyber <zakutailo.pavel@gmail.com>
2026-07-09 22:30:34 +0300

    Refine parent dashboard design

--- haldus-shared.js
+++ haldus-shared.js
@@ -1,5 +1,5 @@
 (function(){
-  const APP_VERSION  = 'CRM 2026-07-09 22:26';
+  const APP_VERSION  = 'CRM 2026-07-09 22:36';
   const LEVELS   = ['Eelkool','A1','A2','B1','B2','C1'];
   const TEACHERS = ['Pavel','Jelena','Elizaveta','Angelina'];
   const STAFF_ALIASES = {
