4d7d6c1d81bf2d2312acf2d0099708b3ac378740
zakutailopavel-cyber <zakutailo.pavel@gmail.com>
2026-08-07 15:08:43 +0300

    export scheduled automatic invoice preview

--- functions/main.js
+++ functions/main.js
@@ -1,7 +1,9 @@
 const existingFunctions = require('./index');
 const { manualInvoiceApi } = require('./manual-invoice-api');
+const { automaticInvoicePreview } = require('./auto-invoice-preview');
 
 module.exports = {
   ...existingFunctions,
   manualInvoiceApi,
+  automaticInvoicePreview,
 };
