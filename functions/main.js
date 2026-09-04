f50a458ed7549135733a75714653c458a647f389
zakutailopavel-cyber <zakutailo.pavel@gmail.com>
2026-09-04 04:02:07 +0300

    feat(learning): export profile evidence API

--- functions/main.js
+++ functions/main.js
@@ -1,9 +1,11 @@
 const existingFunctions = require('./index');
 const { manualInvoiceApi } = require('./manual-invoice-api');
 const { learningSessionApi } = require('./learning-session-api');
+const { learningProfileEvidenceApi } = require('./learning-profile-evidence-api');
 
 module.exports = {
   ...existingFunctions,
   manualInvoiceApi,
   learningSessionApi,
+  learningProfileEvidenceApi,
 };
